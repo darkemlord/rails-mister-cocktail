680a5d4c52e044a4c00031072bd263e42010d67d
emanuel caro <darkemlord@gmail.com>
2021-11-12 23:25:22 +0900

    ingredients seeded!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = JSON.parse(URI.open(url).read)
+drinks = response['drinks']
+puts "Destroying ingredients"
+
+Ingredient.destroy_all
+puts "Adding new ingredients"
+drinks.each do |drink|
+  Ingredient.create!(
+    name: drink['strIngredient1']
+  )
+end
+
+puts "Ingredients added!"
